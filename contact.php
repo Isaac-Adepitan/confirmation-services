9655cb3e301f87366dd4593ac928adfa28e3dff6
Isaac Adepitan <isaacadepitan@gmail.com>
2021-01-05 16:35:44 +0100

    Fixed email pointer

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ $email_body = "Name: $fname.\n".
                "Email: $email.\n".
                "Message: $message.\n";
 
-$to = "isaacadepitan@gmail.com";
+$to = "support@econfirmationservices.com";
 $headers = "From: $email_from \r\n";
 $headers = "Reply-to: $email \r\n";
 
